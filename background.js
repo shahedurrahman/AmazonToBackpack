f2025893cf1e161a8a6d5f932e9f9c44c43f06bc
Shahedur Rahman <shahed.munna@gmail.com>
2015-10-01 01:57:53 +0600

    Added extension source files.

---
+++ background.js
@@ -0,0 +1,22 @@
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {
+                        hostEquals: 'www.amazon.com'
+                    }
+                })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
+
+chrome.pageAction.onClicked.addListener(function (tab) {
+    var amazonUrl = tab.url;
+    var asin = amazonUrl.match("/([a-zA-Z0-9]{10})(?:[/?]|$)");
+    var backpackUrl = 'https://www.backpackbang.com/item?ASIN=' + asin[1];
+
+    chrome.tabs.create({ url: backpackUrl });
+});

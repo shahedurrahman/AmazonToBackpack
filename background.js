415815a9f3aa3cec816a13960f71fa1a85ced9c8
Shahedur Rahman <shahedurrahman@users.noreply.github.com>
2016-06-12 17:55:25 +0600

    Updated backpack url format from # to /

--- background.js
+++ background.js
@@ -17,7 +17,7 @@ chrome.runtime.onInstalled.addListener(function () {
 chrome.pageAction.onClicked.addListener(function (tab) {
     var amazonUrl = tab.url;
     var asin = amazonUrl.match("/([a-zA-Z0-9]{10})(?:[/?]|$)");
-    var backpackUrl = 'https://backpackbang.com/item#' + asin[1];
+    var backpackUrl = 'https://backpackbang.com/item/' + asin[1];
 
     chrome.tabs.create({ url: backpackUrl });
 });
